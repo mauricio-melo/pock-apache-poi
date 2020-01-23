f1dc00dbb49f6307f427eb9b929b2a9a85749cdc
mauricio.melo <mauricio.melo@iteris.com.br>
2020-01-23 16:32:16 -0300

    ajustes

--- src/main/java/com/mmelo/excel/processor/GenerateExcelWithImageCell.java
+++ src/main/java/com/mmelo/excel/processor/GenerateExcelWithImageCell.java
@@ -1,55 +1,87 @@
 package com.mmelo.excel.processor;
 
-import com.mmelo.excel.config.ColumnsConfig;
+import com.mmelo.excel.model.User;
 import lombok.RequiredArgsConstructor;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.util.IOUtils;
 import org.apache.poi.util.Units;
-import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFRichTextString;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class GenerateExcelWithImageCell {
 
-    private static final String fileName = "/home/mauriciomelo/excel/30.xls";
+    private static final String fileName = "/home/mauriciomelo/excel/84.xls";
     private static final String imagePath = "/home/mauriciomelo/excel/rede-logo.png";
 
     public void create(final LocalDate startDate, final LocalDate endDate) throws Exception {
+        //criação da planilha
         final Workbook wb = new XSSFWorkbook();
-
-        final CellStyle styleVertAlingBottom = wb.createCellStyle();
-        styleVertAlingBottom.setVerticalAlignment(VerticalAlignment.BOTTOM);
-
         final Sheet sheet = wb.createSheet();
-        sheet.setColumnWidth(0, 30 * 256); //30 default characters width
 
-        final Row row = sheet.createRow(0);
-        row.setHeight((short)(1200)); //100pt height * 20 = twips (twentieth of an inch point)
+        //criação do header com imagem, titulo e datas
+        createHeader(wb, sheet, startDate, endDate);
 
-        final Cell cell = row.createCell(0);
-        cell.setCellValue("PERIODO: " + startDate.toString() + " A " + endDate.toString()
-                + "\n" + "DATA DE EMISSÃO: " + LocalDate.now().toString());
-        cell.setCellStyle(styleVertAlingBottom);
+        //inserção dos dados
+        insertData(sheet);
 
-        final InputStream is = new FileInputStream(imagePath);
-        final int pictureIdx = wb.addPicture(IOUtils.toByteArray(is), Workbook.PICTURE_TYPE_JPEG);
-        is.close();
-
-        insertImage((XSSFSheet)sheet, pictureIdx);
-
+        //criação do arquivo
         final FileOutputStream out = new FileOutputStream(new File(GenerateExcelWithImageCell.fileName));
         wb.write(out);
         out.close();
     }
 
-    private void insertImage(final XSSFSheet sheet, final int pictureIdx) throws Exception {
+    public void createHeader(final Workbook wb, final Sheet sheet, final LocalDate startDate, final LocalDate endDate) throws Exception {
+
+        //largura das colunas que serão mescladas
+        sheet.setColumnWidth(0, 30 * 130); //15 default characters width
+        sheet.setColumnWidth(1, 30 * 130); //15 default characters width
+
+        //altura da linha
+        final Row row = sheet.createRow(0);
+        row.setHeight((short)(1400)); //100pt height * 20 = twips (twentieth of an inch point)
+
+        // criação do texto
+        Font fontBold = wb.createFont();
+        fontBold.setFontHeight((short) 200);
+        fontBold.setFontName("Liberation Sans");
+        fontBold.setBold(true);
+        RichTextString richString = new XSSFRichTextString("EXTRATO PARA SIMPLES CONFERÊNCIA"
+                + "\n" + "PERIODO: " + startDate.toString() + " A " + endDate.toString()
+                + "\n" + "DATA DE EMISSÃO: " + LocalDate.now().toString());
+        richString.applyFont(0, 32, fontBold);
+
+        //inserção do texto na celular, com alinhamento inferior
+        final Cell cell = row.createCell(0);
+        cell.setCellValue(richString);
+        final CellStyle styleBottom = wb.createCellStyle();
+        styleBottom.setVerticalAlignment(VerticalAlignment.BOTTOM);
+        cell.setCellStyle(styleBottom);
+
+        //mescla das celulas
+        sheet.addMergedRegion(new CellRangeAddress(0,0,0,1));
+
+        //inserção da imagem
+        insertImage((XSSFSheet)sheet, wb);
+    }
+
+    private void insertImage(final XSSFSheet sheet, final Workbook wb) throws Exception {
+        //captura da imagem
+        final InputStream is = new FileInputStream(imagePath);
+        final int pictureIdx = wb.addPicture(IOUtils.toByteArray(is), Workbook.PICTURE_TYPE_JPEG);
+        is.close();
 
         final CreationHelper helper = sheet.getWorkbook().getCreationHelper();
         final Drawing drawing = sheet.createDrawingPatriarch();
@@ -65,7 +97,35 @@ public class GenerateExcelWithImageCell {
         anchor.setDx2(Units.pixelToEMU(80)); //dx = left + wanted width in px
         anchor.setDy2(Units.toEMU(22)); //dy= top + wanted height in pt
 
-        drawing.createPicture( anchor, pictureIdx);
+        drawing.createPicture(anchor, pictureIdx);
+    }
+
+    private void insertData(Sheet sheet) {
+        List<User> listUser = new ArrayList<>();
+        listUser.add(new User("Mauricio", "Aprovado"));
+        listUser.add(new User("Raphael",  "Aprovado"));
+        listUser.add(new User("Alexandre",  "Aprovado"));
+
+        int rownum = 1;
+        Row row = sheet.createRow(rownum);
+
+        Cell cellSer1 = row.createCell(0);
+        String stringCellValueSer1 = "Nome";
+        cellSer1.setCellValue(stringCellValueSer1);
+
+        Cell cellnf1 = row.createCell(1);
+        String stringCellValue1 = "Status";
+        cellnf1.setCellValue(stringCellValue1);
+
+        rownum ++;
+        for (User user : listUser) {
+            Row row1 = sheet.createRow(rownum++);
+            int cellnum = 0;
+            Cell cellNome = row1.createCell(cellnum++);
+            cellNome.setCellValue(user.getNome());
+            Cell cellRa = row1.createCell(cellnum++);
+            cellRa.setCellValue(user.getStatus());
+        }
     }
 
 }
